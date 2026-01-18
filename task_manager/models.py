b561b1ba6d6be30bd1ba15e1477766fdc81ef29f
Jiri-Blazek <blazek.j@centrum.cz>
2026-01-18 20:45:14 +0100

    Update of diagrams+html

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    number = models.PositiveSmallIntegerField()
+
+
+class Configuration(models.Model):
+    program = models.CharField(max_length=50)
+    file_name = models.CharField(max_length=50)
+    working_directory = models.CharField(max_length=200)
+    additional_parameters = models.CharField(max_length=100, blank=True)
+
+
+class System_resources(models.Model):
+    cpu = models.PositiveSmallIntegerField()
+    memory = models.PositiveSmallIntegerField()
+    is_running = models.BooleanField()
+
+
+class Schedule(models.Model):
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField()
